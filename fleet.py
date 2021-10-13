34602aed0c28e396db3cdae731f861c9a037b94a
andrew morales <andrew.morales725@gmail.com>
2021-10-13 12:48:55 -0400

    Updated create_fleet, create_herd methods

--- fleet.py
+++ fleet.py
@@ -5,5 +5,14 @@ class Fleet:
         self.robots = []
 
     def create_fleet(self):
-        # no return
-        pass
+        robot_1 = Robot('bot1')
+        robot_1.health = 500
+        robot_1.weapon('Gattling Gun',100)
+
+        robot_2 = Robot('bot2')
+        robot_2.health = 250
+        robot_1.weapon('Blaster',75)
+
+        robot_3 = Robot('bot3')
+        robot_3.health = 300
+        robot_1.weapon('Power Fist',70)
--- herd.py
+++ herd.py
@@ -6,13 +6,13 @@ class Herd:
 
     def create_herd(self):
 
-        dino_1 = Dinosaur('T-rex, 100')
+        dino_1 = Dinosaur('T-rex', 100)
         dino_1.health = 500
 
-        dino_2 = Dinosaur('Velociraptor, 75')
+        dino_2 = Dinosaur('Velociraptor', 75)
         dino_2.health = 250
 
-        dino_3 = Dinosaur('Ankylosaurus, 70')
+        dino_3 = Dinosaur('Ankylosaurus', 70)
         dino_3.health = 300
 
 
