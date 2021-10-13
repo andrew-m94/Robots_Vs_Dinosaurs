24f4c74e05745ceb65161744d5c431902238e686
andrew morales <andrew.morales725@gmail.com>
2021-10-13 15:48:48 -0400

    Updated battle method

--- battlefield.py
+++ battlefield.py
@@ -15,8 +15,24 @@ class Battlefield:
         pass
 
     def battle(self):
-        # no return
-        pass
+        dino_element = 0
+        robo_element = 0
+
+        while len(self.fleet.robots) != 0 or len(self.herd.dinosaurs) != 0:
+            
+            if dino_element >= len(self.herd.dinosaurs):
+                dino_element = 0
+
+            self.dino_turn(self.herd.dinosaurs[dino_element])
+            dino_element += 1
+           
+            if len(self.fleet.robots) != 0:
+
+                if robo_element >= len(self.fleet.robots):
+                    robo_element = 0
+
+                self.robo_turn(self.fleet.robots[robo_element])
+                robo_element += 1
 
     def dino_turn(self, dinosaur):
         self.show_robo_opponent_options()
@@ -24,11 +40,17 @@ class Battlefield:
 
         dinosaur.attack(self.fleet.robots[opponent-1])
 
+        if self.fleet.robots[opponent - 1].health <= 0:
+            self.fleet.robots.remove(self.fleet.robots[opponent - 1])
+
     def robo_turn(self, robot):
         self.show_dino_opponent_options()
         opponent = int(input('Choose which dinosaur to attack: #'))
 
         robot.attack(self.herd.dinosaurs[opponent-1])
+
+        if self.herd.dinosaurs[opponent - 1].health <= 0:
+            self.herd.dinosaurs.remove(self.herd.dinosaurs[opponent - 1])
 
     def show_dino_opponent_options(self):
         element = 0
--- main.py
+++ main.py
@@ -2,7 +2,5 @@ from battlefield import Battlefield
 
 #For testing
 battlefield1 = Battlefield()
-battlefield1.dino_turn(battlefield1.herd.dinosaurs[0])
-battlefield1.robo_turn(battlefield1.fleet.robots[0])
-battlefield1.dino_turn(battlefield1.herd.dinosaurs[1])
-battlefield1.robo_turn(battlefield1.fleet.robots[1])
+
+battlefield1.battle()
